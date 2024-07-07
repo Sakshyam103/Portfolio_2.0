6169ebd68a2c8dc2368647a3391851930dc0abeb
Sakshyam <ssarki@oswego.edu>
2024-07-07 16:28:16 -0400

    Halfway

--- Portfolio/src/Home.jsx
+++ Portfolio/src/Home.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import {useNavigate} from "react-router-dom";
 import {animate} from "framer-motion";
 import {motion} from "framer-motion";
+
 function Home(){
     const navigate = useNavigate();
     return(
@@ -12,7 +13,7 @@ function Home(){
                     {/*    <button>previous button</button>*/}
                     {/*</div>*/}
                     <div id="chapter-1" className="opacity-90 m-auto w-[50%] max-[1024px]:w-[80%] max-[480px]:w-[100%] grid grid-cols-3 grid-rows-[1fr,auto,1fr,1fr,1fr] gap-2 bg-none border-black border-2 p-3 rounded-lg shadow-xl shadow-black">
-                        <div className="col-span-3 border-2 border-black rounded-lg hover:shadow-2xl text-purple-700 hover:text-purple-900 hover:shadow-black bg-black"><button className="h-52 md:h-60 lg:h-72 xl:h-72 w-full bg-black relative" onClick={()=>navigate("home/about_me")}><img src={"/src/assets/IMG_7165.PNG"} alt="pic1" className="h-full w-full"/><div className="absolute font-serif font-bold italic text-3xl max-[1350px]:text-3xl max-[480px]:text-lg  top-3 right-2">About me</div></button>
+                        <div className="col-span-3 border-2 border-black rounded-lg hover:shadow-2xl text-purple-700 hover:text-purple-900 hover:shadow-black bg-black"><button className="h-52 md:h-60 lg:h-72 xl:h-72 w-full bg-black relative" onClick={()=>navigate("home/about_me")}><img src='../src/assets/IMG_7167.PNG' alt="pic1" className="h-full w-full"/><div className="absolute font-serif font-bold italic text-3xl max-[1350px]:text-3xl max-[480px]:text-lg  top-3 right-2">About me</div></button>
                         </div>
                         <div className="h-3 col-span-3 justify-self-center content-center pl-3 font-bold font-serif italic text-white">Chapter 1: Introduction</div>
                         <div className="col-span-3 border-2 border-black rounded-lg hover:shadow-2xl hover:shadow-black bg-black"><button className="h-52 md:h-60 lg:h-72 xl:h-72 w-full bg-black text-purple-700 hover:text-purple-900 relative" onClick={()=>navigate("home/work_experience")}><img src={"/src/assets/IMG_7167.PNG"} alt="pic2" className="h-full w-full object-fill"/><div className="absolute font-serif font-bold italic text-3xl max-[1350px]:text-3xl max-[480px]:text-lg top-3 right-2">Projects</div></button></div>
